4c998d5bfac36a1216733cef3dfe21eb7c95378e
Joselyn~ <joojedavar@gmail.com>
2026-04-30 17:55:20 -0600

    Cambios v2 del vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs' // Importamos esto para leer archivos del sistema
+
+// Detectamos si estamos dentro de Docker buscando su archivo oculto
+const isDocker = fs.existsSync('/.dockerenv');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // 1. OBLIGATORIO PARA DOCKER: Permite que la página salga del contenedor
+    // 1. OBLIGATORIO PARA DOCKER
     host: true, 
-    
     allowedHosts: true,
+    
     proxy: {
-      // 2. CAMBIO CLAVE: Cambiamos "localhost" por "backend"
-      '/api': 'http://localhost:3000'
+      // 2. MAGIA: Decide automáticamente qué ruta usar
+      '/api': isDocker ? 'http://backend:3000' : 'http://localhost:3000'
     }
   }
 })
