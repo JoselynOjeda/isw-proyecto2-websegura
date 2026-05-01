3ff4c228311dfea2774f594814c123c0b1795690
Joselyn~ <joojedavar@gmail.com>
2026-04-30 18:07:56 -0600

    funcionamiento del localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs' // Importamos esto para leer archivos del sistema
-
-// Detectamos si estamos dentro de Docker buscando su archivo oculto
-const isDocker = fs.existsSync('/.dockerenv');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // 1. OBLIGATORIO PARA DOCKER
     host: true, 
     allowedHosts: true,
-    
     proxy: {
-      // 2. MAGIA: Decide automáticamente qué ruta usar
-      '/api': isDocker ? 'http://backend:3000' : 'http://localhost:3000'
+      '/api': 'http://localhost:3000'
     }
   }
 })
